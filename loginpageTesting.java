05aa88d549c22def2d892f869b0cdf196ebe9b52
Roja Kundar <kundarroja@gmail.com>
2024-12-21 19:25:34 +0530

    Add new code For logout

--- loginpageTesting.java
+++ loginpageTesting.java
@@ -6,16 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://practicetestautomation.com/practice-test-login/");
-		driver.findElement(By.id("username")).sendKeys("studet");
-		driver.findElement(By.id("password")).sendKeys("Password");
+		driver.findElement(By.id("username")).sendKeys("student");
+		driver.findElement(By.id("password")).sendKeys("Password123");
 		driver.findElement(By.className("btn")).click();
-		System.out.println(driver.findElement(By.id("error")).getText());
-		driver.close();
+//		System.out.println(driver.findElement(By.id("error")).getText());
+		System.out.println(driver.findElement(By.id("loop-container")).getText());
+		driver.findElement(By.linkText("Log out")).click();
+		driver.quit();
 	}
-
 }
